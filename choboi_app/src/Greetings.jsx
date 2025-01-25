3c963317e5aebb6a7193d7376bd9996818b05664
Muhammad Abba <muhammadabba9741@gmail.com>
2025-01-25 22:36:09 +0100

    choboi

---
+++ choboi_app/src/Greetings.jsx
@@ -0,0 +1,18 @@
+function Greeting () {
+    return <h1> &quot;I Abba Muhammad, I will end you.&quot;</h1>;
+    function App() {
+        return (
+          <div>
+            <h1>Animals: </h1>
+            <ul>
+              <li>Lion</li>
+              <li>Cow</li>
+              <li>Snake</li>
+              <li>Lizard</li>
+            </ul>
+          </div>
+        );
+      }      
+}
+
+export default Greeting;
